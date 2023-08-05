ad99e3bf1b757c14e96c1e04ec279d1515077398
n3rddd <119649988+n3rddd@users.noreply.github.com>
2023-08-05 17:29:28 +0800

    Add files via upload

--- JN/EXT/OPENJS/open/lib/utils.js
+++ JN/EXT/OPENJS/open/lib/utils.js
@@ -26,11 +26,55 @@ function removeExt(text) {
 }
 
 async function log(str) {
-    // let url = 'remote log url';
-    // await req(url, {
-    //     method: 'post',
-    //     data: {log: str}
-    // });
+    console.debug(str);
 }
 
-export { isSub, getSize, removeExt, log};
+function isVideoFormat(url) {
+    var RULE = /http((?!http).){12,}?\.(m3u8|mp4|flv|avi|mkv|rm|wmv|mpg|m4a|mp3)\?.*|http((?!http).){12,}\.(m3u8|mp4|flv|avi|mkv|rm|wmv|mpg|m4a|mp3)|http((?!http).)*?video\/tos*/;
+    if (url.indexOf("url=http") > -1 || url.indexOf(".js") > -1 || url.indexOf(".css") > -1 || url.indexOf(".html") > -1) {
+        return false;
+    }
+    return RULE.test(url);
+}
+
+function jsonParse(input, json) {
+    var jsonPlayData = JSON.parse(json);
+    var url = jsonPlayData.url;
+    if (url.startsWith("//")) {
+        url = "https:" + url;
+    }
+    if (!url.startsWith("http")) {
+        return null;
+    }
+    if (url === input) {
+        if (!isVideoFormat(url)) {
+            return null;
+        }
+    }
+    var headers = {};
+    var ua = jsonPlayData["user-agent"] || "";
+    if (ua.trim().length > 0) {
+        headers["User-Agent"] = " " + ua;
+    }
+    var referer = jsonPlayData.referer || "";
+    if (referer.trim().length > 0) {
+        headers["Referer"] = " " + referer;
+    }
+    var taskResult = {
+        header: headers,
+        url: url
+    };
+    return taskResult;
+}
+
+function debug(obj) {
+    for (var a in obj) {
+        if (typeof(obj[a]) == "object") {
+            debug(obj[a]); //递归遍历
+        } else {
+            console.debug(a + "=" + obj[a]);
+        }
+    }
+}
+
+export { isSub, getSize, removeExt, log, isVideoFormat, jsonParse, debug};
