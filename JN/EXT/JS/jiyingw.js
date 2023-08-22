fac95071d6390b509ed3c1e6a46b0c5410e33033
n3rddd <119649988+n3rddd@users.noreply.github.com>
2023-08-22 21:04:48 +0800

    Add files via upload

--- JN/EXT/JS/jiyingw.js
+++ JN/EXT/JS/jiyingw.js
@@ -1,5 +1,8 @@
 var rule = {
 	title:'极影网[磁]',
+	//host:'https://www.jiyingw.net',
+	//homeUrl:'/',
+	//url: '/fyclass/page/fypage?',
 	host:'http://127.0.0.1:10079',
 	homeUrl:'/p/0/socks5%253A%252F%252F192.168.101.1%253A1080/https://www.jiyingw.net',
 	url: '/p/0/socks5%253A%252F%252F192.168.101.1%253A1080/https://www.jiyingw.net/fyclass/page/fypage?',
@@ -38,77 +41,132 @@ var rule = {
 		tabs:`js:
 pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;
 TABS=[]
-var d = pdfa(html, '.movie-introduce p');
-var index=0;
+let tabsa = [];
+let tabsq = [];
+let tabsm = false;
+let tabse = false;
+let d = pdfa(html, '.movie-introduce p a');
 d.forEach(function(it) {
-	let burl = pdfh(it, 'a[href^="magnet"]&&href');
-	if (burl.startsWith("magnet")){
-		log("jiyingw burl >>>>>>" + burl);
-		index = index + 1;
-		let result = 'magnet' + index;
-		TABS.push(result);
+	let burl = pdfh(it, 'a&&href');
+	if (burl.startsWith("https://www.aliyundrive.com/s/")){
+		tabsa.push("阿里云盤");
+	}else if (burl.startsWith("https://pan.quark.cn/s/")){
+		tabsq.push("夸克云盤");
+	}else if (burl.startsWith("magnet")){
+		tabsm = true;
+	}else if (burl.startsWith("ed2k")){
+		tabse = true;
 	}
 });
-if (index === 0){
-	d = pdfa(html, 'div#down p.down-list3');
-	index=0;
-	d.forEach(function(it) {
-		let burl = pdfh(it, 'a[href^="magnet"]&&href');
-		if (burl.startsWith("magnet")){
-			log("jiyingw burl >>>>>>" + burl);
-			index = index + 1;
-			let result = 'magnet' + index;
-			TABS.push(result);
-		}
-	});
+d = pdfa(html, 'div#down p.down-list3 a');
+d.forEach(function(it) {
+	let burl = pdfh(it, 'a&&href');
+	if (burl.startsWith("https://www.aliyundrive.com/s/")){
+		tabsa.push("阿里云盤");
+	}else if (burl.startsWith("https://pan.quark.cn/s/")){
+		tabsq.push("夸克云盤");
+	}else if (burl.startsWith("magnet")){
+		tabsm = true;
+	}else if (burl.startsWith("ed2k")){
+		tabse = true;
+	}
+});
+if (tabsm === true){
+	TABS.push("磁力");
 }
-
+if (tabse === true){
+	TABS.push("電驢");
+}
+let tmpIndex;
+tmpIndex=1;
+tabsa.forEach(function(it){
+	TABS.push(it + tmpIndex);
+	tmpIndex = tmpIndex + 1;
+});
+tmpIndex=1;
+tabsq.forEach(function(it){
+	TABS.push(it + tmpIndex);
+	tmpIndex = tmpIndex + 1;
+});
 log('jiyingw TABS >>>>>>>>>>>>>>>>>>' + TABS);
 `,
 		lists:`js:
 log(TABS);
 pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;
 LISTS = [];
-var d = pdfa(html, '.movie-introduce p');
-var secondinit=false;
-TABS.forEach(function(tab) {
-	log('jiyingw tab >>>>>>>>' + tab);
-	if (/^magnet/.test(tab)) {
-		let targetindex = parseInt(tab.substring(6));
-		let index = 0;
-		d.forEach(function(it){
-			let burl = pdfh(it, 'a[href^="magnet"]&&href');
-			if (burl.startsWith("magnet")){
-				index = index + 1;
-				if (index === targetindex){
-					let title = pdfh(it, 'a[href^="magnet"]&&Text');
-					log('jiyingw title >>>>>>>>>>>>>>>>>>>>>>>>>>' + title);
-					log('jiyingw burl >>>>>>>>>>>>>>>>>>>>>>>>>>' + burl);
-					let loopresult = title + '$' + burl;
-					LISTS.push([loopresult]);
-				}
-			}
-		});
-		if (index === 0){
-			if (secondinit === false){
-				d = pdfa(html, 'div#down p.down-list3');
-				secondinit = true;
-			}
-			d.forEach(function(it){
-				let burl = pdfh(it, 'a[href^="magnet"]&&href');
-				if (burl.startsWith("magnet")){
-					index = index + 1;
-					if (index === targetindex){
-						let title = pdfh(it, 'a[href^="magnet"]&&Text');
-						log('jiyingw title >>>>>>>>>>>>>>>>>>>>>>>>>>' + title);
-						log('jiyingw burl >>>>>>>>>>>>>>>>>>>>>>>>>>' + burl);
-						let loopresult = title + '$' + burl;
-						LISTS.push([loopresult]);
-					}
-				}
-			});
+let lista = [];
+let listq = [];
+let listm = [];
+let liste = [];
+let d = pdfa(html, '.movie-introduce p');
+d.forEach(function(it){
+	let burl = pdfh(it, 'a&&href');
+	let title = pdfh(it, 'a&&Text');
+	log('dygang title >>>>>>>>>>>>>>>>>>>>>>>>>>' + title);
+	log('dygang burl >>>>>>>>>>>>>>>>>>>>>>>>>>' + burl);
+	let loopresult = title + '$' + burl;
+	if (burl.startsWith("https://www.aliyundrive.com/s/")){
+		if (TABS.length==1){
+			burl = "http://127.0.0.1:9978/proxy?do=ali&type=push&confirm=0&url=" + encodeURIComponent(burl);
+		}else{
+			burl = "http://127.0.0.1:9978/proxy?do=ali&type=push&url=" + encodeURIComponent(burl);
 		}
+		loopresult = title + '$' + burl;
+		lista.push(loopresult);
+	}else if (burl.startsWith("https://pan.quark.cn/s/")){
+		if (TABS.length==1){
+			burl = "http://127.0.0.1:9978/proxy?do=quark&type=push&confirm=0&url=" + encodeURIComponent(burl);
+		}else{
+			burl = "http://127.0.0.1:9978/proxy?do=quark&type=push&url=" + encodeURIComponent(burl);
+		}
+		loopresult = title + '$' + burl;
+		listq.push(loopresult);
+	}else if (burl.startsWith("magnet")){
+		listm.push(loopresult);
+	}else if (burl.startsWith("ed2k")){
+		liste.push(loopresult);
 	}
+});
+d = pdfa(html, 'div#down p.down-list3 a');
+d.forEach(function(it){
+	let burl = pdfh(it, 'a&&href');
+	let title = pdfh(it, 'a&&Text');
+	log('dygang title >>>>>>>>>>>>>>>>>>>>>>>>>>' + title);
+	log('dygang burl >>>>>>>>>>>>>>>>>>>>>>>>>>' + burl);
+	let loopresult = title + '$' + burl;
+	if (burl.startsWith("https://www.aliyundrive.com/s/")){
+		if (TABS.length==1){
+			burl = "http://127.0.0.1:9978/proxy?do=ali&type=push&confirm=0&url=" + encodeURIComponent(burl);
+		}else{
+			burl = "http://127.0.0.1:9978/proxy?do=ali&type=push&url=" + encodeURIComponent(burl);
+		}
+		loopresult = title + '$' + burl;
+		lista.push(loopresult);
+	}else if (burl.startsWith("https://pan.quark.cn/s/")){
+		if (TABS.length==1){
+			burl = "http://127.0.0.1:9978/proxy?do=quark&type=push&confirm=0&url=" + encodeURIComponent(burl);
+		}else{
+			burl = "http://127.0.0.1:9978/proxy?do=quark&type=push&url=" + encodeURIComponent(burl);
+		}
+		loopresult = title + '$' + burl;
+		listq.push(loopresult);
+	}else if (burl.startsWith("magnet")){
+		listm.push(loopresult);
+	}else if (burl.startsWith("ed2k")){
+		liste.push(loopresult);
+	}
+});
+if (listm.length>0){
+	LISTS.push(listm);
+}
+if (liste.length>0){
+	LISTS.push(liste);
+}
+lista.forEach(function(it){
+	LISTS.push([it]);
+});
+listq.forEach(function(it){
+	LISTS.push([it]);
 });
 `,
 
