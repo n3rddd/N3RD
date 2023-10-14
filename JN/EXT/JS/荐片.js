7bf15c29ee4c1f763d84c4144f9ee4e935a8c737
n3rddd <119649988+n3rddd@users.noreply.github.com>
2023-10-14 09:32:04 +0800

    Update 荐片.js

--- JN/EXT/JS/荐片.js
+++ JN/EXT/JS/荐片.js
@@ -1,6 +1,6 @@
 var rule = {
     title:'荐片',
-    host:'http://yjpapipxblwdohpakljwg.hxhzs.com',
+    host:'http://api2.rinhome.com',
     //homeUrl:'https://yjpapipxblwdohpakljwg.hxhzs.com/api/tag/hand?code=unknown601193cf375db73d&channel=wandoujia',//网站的首页链接,用于分类获取和推荐获取
  homeUrl:'https://api2.rinhome.com/api/tag/hand?code=unknown601193cf375db73d&channel=wandoujia',
 	url:'/api/crumb/list?area=0&category_id=fyclass&page=fypage&type=0&limit=24&fyfilter',
