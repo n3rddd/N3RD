3db3b6eee9f5278aeb84d84b671758cef983765a
n3rddd <119649988+n3rddd@users.noreply.github.com>
2024-02-12 20:56:08 +0800

    Add files via upload

--- JN/EXT/JS/18哈.js
+++ JN/EXT/JS/18哈.js
@@ -1,14 +1,12 @@
-muban.短视2.二级.title = '.slide-info-title&&Text;.slide-info:eq(0)&&Text';
 muban.短视2.二级.img = '.lazy&&data-src';
-muban.短视2.二级.desc = '.fraction&&Text;;;.slide-info:eq(3)--strong&&Text;.slide-info:eq(2)--strong&&Text';
 muban.短视2.二级.tab_text = 'body--i--span&&Text';
 var rule={
-    title:'7喜影院',
+    title:'18哈',
     模板:'短视2',
-    host:'https://7xi.tv',
+    host:'https://sk100.tv',
     detailUrl:'/voddetail/fyid.html',
-	class_name:'电影&短剧&4K电影&电视剧&综艺&动漫&电影解说&短视频',
-    class_url:'1&56&57&2&3&4&25&53',
+	class_name:'电影&电视剧&综艺&动漫&爽文短剧',
+    class_url:'1&2&4&3&33',
     lazy:`js:
         var html = JSON.parse(request(input).match(/r player_.*?=(.*?)</)[1]);
         var url = html.url;
