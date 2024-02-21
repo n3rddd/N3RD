bd2b5db633052d2cf4ff338bc5b62e9625e0195c
n3rddd <119649988+n3rddd@users.noreply.github.com>
2024-02-21 16:12:56 +0800

    Update 骚火电影.js

--- JN/EXT/JS/骚火电影.js
+++ JN/EXT/JS/骚火电影.js
@@ -2,7 +2,7 @@
 var rule={
     title:'骚火电影',
     // host:'https://shdy3.com',
-    //host:'http://shapp.us',
+    // host:'http://shapp.us',
     host:'https://shdy2.com',
     hostJs:'print(HOST);let html=request(HOST,{headers:{"User-Agent":PC_UA}});let src=jsp.pdfh(html,"a:eq(2)&&href");print(src);HOST=src',
     // url:'/list/fyclass-fypage.html',
