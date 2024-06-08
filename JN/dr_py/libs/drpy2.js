b79504f03e5e16c7bc5e1334b11b3ba83ec0b830
n3rddd <119649988+n3rddd@users.noreply.github.com>
2024-06-08 18:52:41 +0800

    Add files via upload

--- JN/dr_py/libs/drpy2.js
+++ JN/dr_py/libs/drpy2.js
@@ -264,7 +264,7 @@ function pre() {
 
 let rule = {};
 let vercode = typeof (pdfl) === 'function' ? 'drpy2.1' : 'drpy2';
-const VERSION = vercode + ' 3.9.50beta26 20240605';
+const VERSION = vercode + ' 3.9.50beta27 20240608';
 /** 已知问题记录
  * 1.影魔的jinjia2引擎不支持 {{fl}}对象直接渲染 (有能力解决的话尽量解决下，支持对象直接渲染字符串转义,如果加了|safe就不转义)[影魔牛逼，最新的文件发现这问题已经解决了]
  * Array.prototype.append = Array.prototype.push; 这种js执行后有毛病,for in 循环列表会把属性给打印出来 (这个大毛病需要重点排除一下)
@@ -2946,7 +2946,7 @@ function proxyParse(proxyObj) {
         log('准备执行本地代理规则:\n' + proxyObj.proxy_rule);
         try {
             eval(proxyObj.proxy_rule);
-            if (input && input !== proxyObj.params && Array.isArray(input) && input.length === 3) {
+            if (input && input !== proxyObj.params && Array.isArray(input) && input.length >= 3) {
                 return input
             } else {
                 return [404, 'text/plain', 'Not Found']
