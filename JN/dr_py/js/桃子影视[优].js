274fe47d86246ceb794dea1e21c9b24e79e400bc
n3rddd <119649988+n3rddd@users.noreply.github.com>
2024-07-13 21:53:37 +0800

    Update 桃子影视[优].js

--- JN/dr_py/js/桃子影视[优].js
+++ JN/dr_py/js/桃子影视[优].js
@@ -8,16 +8,15 @@ var rule = {
     host: 'https://www.taozi007.com',
     "url": "/show/fyfilter.html",
     class_parse: '.navbar-items&&a[href*=type];a&&title;a&&href;(\\d+).html',
-    headers: {
-        headers: {
-'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
-'Cookie': '__51vcke__KOfCv4E7m6sGPKS0=4f73f3a8-6b11-584a-b03d-43ed9ede5dc7; __51vuft__KOfCv4E7m6sGPKS0=1720351788356; mx_style=black; guardok=Z0MF4m7EM/sJ51hJZTTVcZVu1TalQjZwqzSVq0Sr6K8vT+2kVxKwaNmBvPWiLXTA1lVp/mYIgXLj2sIaJGyTqA==; __vtins__KOfCv4E7m6sGPKS0=%7B%22sid%22%3A%20%2241be730c-7539-541e-a734-2f5a3f9ee380%22%2C%20%22vd%22%3A%201%2C%20%22stt%22%3A%200%2C%20%22dr%22%3A%200%2C%20%22expires%22%3A%201720827935086%2C%20%22ct%22%3A%201720826135086%7D; __51uvsct__KOfCv4E7m6sGPKS0=6',
-            },
+     headers: {
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 13; PJB110 Build/TP1A.220905.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/103.0.5060.129 Mobile Safari/537.36',
+        'Cookie': '__51uvsct__KOfCv4E7m6sGPKS0=1; __51vcke__KOfCv4E7m6sGPKS0=9448bdca-ccb0-5f71-a7bb-798f2815c976; __51vuft__KOfCv4E7m6sGPKS0=1720349008602; mx_style=black; __vtins__KOfCv4E7m6sGPKS0=%7B%22sid%22%3A%20%228f0861a4-3d55-576c-91f9-2a391c7a240a%22%2C%20%22vd%22%3A%205%2C%20%22stt%22%3A%2031693%2C%20%22dr%22%3A%2012623%2C%20%22expires%22%3A%201720350840266%2C%20%22ct%22%3A%201720349040266%7D; guardok=rYTLhNjCz3jZld2Aa0UWOG4Y+oTV+hFwk/wcLQpM6dHFCbKGjsa89dYPe4+8nxtJtwLVTGAp/Dkxw2Qf5vGnyA==; showBtn=true'，
+    },
     lazy: $js.toString(() => {
         var html = JSON.parse(request(input).match(/r player_.*?=(.*?)</)[1]);
         var url = html.url;
         if (html.from != 'ty') {
-            if (/\.m3u8|\.mp4/.test(url)) {
+          if (/\.m3u8|\.mp4/.test(url)) {
                 input = {
                     jx: 0,
                     url: url,
