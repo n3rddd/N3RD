8e8c6839a6efdd0afd2a12500235ce717ef0fb0f
n3rddd <119649988+n3rddd@users.noreply.github.com>
2024-07-13 12:16:38 +0800

    Add files via upload

---
+++ JN/dr_py/js/全看网[自动].js
@@ -0,0 +1,17 @@
+var rule = {
+    模板: '自动',
+    模板修改: $js.toString(() => {
+        Object.assign(muban.自动.二级, {
+            tabs: '.stui-pannel__head h3',
+      });
+    }),
+    title: '全看网[自动]',
+    host: 'https://42c37e0b590eefa75c522370fc323f39.91qkw.cc',
+    url: '/show/fyclass--------fypage---.html',
+   searchUrl: '/search/**----------fypage---.html',
+    搜索: 'ul.stui-vodlist&&li;a&&title;.lazyload&&data-original;.text-right&&Text;a&&href',
+    }
+
+
+
+
