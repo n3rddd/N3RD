9369d435445e2af5aa9816f66199ea174dd0bdd3
n3rddd <119649988+n3rddd@users.noreply.github.com>
2024-07-18 21:20:46 +0800

    Update 剧圈圈.js

--- JN/dr_py/js/剧圈圈.js
+++ JN/dr_py/js/剧圈圈.js
@@ -1,12 +1,51 @@
 Object.assign(muban.mxpro.二级, {
-    tab_text: 'div--small&&Text',
-});
+    "desc": ".module-info-item:eq(1)&&Text;.module-info-item:eq(2)&&Text;.module-info-item:eq(3)&&Text;;",
+    "content": ".module-info-introduction-content&&Text",
+    "tabs": "body&&.module-tab-item",
+    "lists": "body&&.module-play-list:eq(#id)&&a"
+})
 var rule = {
-    模板: 'mxpro',
-    title: '剧圈圈',
-    host: 'https://www.jqqzx.cc/',
-    url: '/vodshow/id/fyclass/page/fypage.html',
-    searchUrl: '/vodsearch**/page/fypage.html',
-    class_parse: '.navbar-items li:gt(1):lt(8);a&&Text;a&&href;.*/(.*?)\.html',
-    cate_exclude: '今日更新|热榜',
+    模板: "mxpro",
+    title: "剧圈圈",
+    host: "https://jqqzx.cc",
+    url: "/vodshowfyfilter.html",
+    searchUrl: "/index.php/ajax/suggest?mid=1&wd=**&limit=50",
+    filterable: 1,
+    filter: "H4sIAAAAAAAAAO1b21LiWBT9F56tIlGR2B8xPzDVD2llNLTijMpUa1dX4f3aopbiZfAyo4jaXrAvjkIBP8NJ4C8meA47++xQGqtsR6vymLVWn71Z53TYi8SPgW5Dj40YsZ7Am18/Bt5HRgJvAtZVge0uBFoCMb0/gq//1PvikTthzIbZ1HFt4rgO2xdBozuoBj61CGr+uFJMW3MziO0eiPWMxgd+t+s5ulSazWVl3QcjGpdE1uyVOTEli3TjD7tnWZZdYbcFWfY+0hvXY5LMHFs2Eykis1t7Jxe123IVjcZdRc3ZzUp+TpaN9uqx0d4IUdqWWGsFtyV2g1z3tq7kG8CLOxsA1/dsABT6JTL8W5/xoVGmq08fGgo2QNl4WSMw2XZZIjDZS1kiMPkkkEIckzeOFOKYvBtkFY7JO0F64RgUmr+0iqekEMfkXXK1W8dAkplxtSsw6OX8qFLaJ71wDFaZXq1tnZBVOAar7J7aH4CswrFHbIA5fmalVoiEYyCZmDfHt4mEY9Bu8rB6QDeAYyBZmmbJr0TCMZBcXLP8OZFwDCTLi2aCHkyONSTV8rK9JbJEYLDKZNn6kiGrcAwOQyHJpm7IYeBYQ1LbWTU3ySoCA+tSM9W5PLGOY9BL6cJa+8GKV6QdgKGj/CkrrqM7hGgKYBAWV6xCutmiEiPdUdI5tphHd5TGtZc7ilVaYn8VG5X0wYgeFBA4tXdCFQICozYyZvpMUggIFN9ydA0BNRSVm3P7sna0Zd5cSjqJkNUsmTNvS03UgnA2Msfm99nO33IDgMInPTkzdzLVTLmS35Y/LyZg7z/nWP5I0gkIDvPCFf3UAnJ8WXf7so4VrPQvVQjIua3tmwtldpiVRYBCN5mylTy35rbkhgAFb8uTVnHDTJFtABTqTl2ziwm5KIekw3n7vVIoosPZuPZyOFuV1vZGgZGIPhi8AxDbRtk2zLZSthWzKmVVzCqUVRCrdhLWBhCrUVbDbJiyYcx2ULYDsyHKhjBLvVKxVyr1SsVeqdQrFXulUq9U7JVKvVKxVwr1SsFeKdQrBXulUK8U7JVCvVKwVwr1SsFeKdQrBXulUK8U7JVCvVKwVwr1SsFeKdQrxfFK7eyUvboDEKtRVsNsmLJhzHZQtgOzIcqGMNtO2XbMtlG2DbOtlG3FrEpZFbMKZbFXGvVKw15p1CsNe6VRrzTslUa90rBXGvVKw15p1CsNe6VRrzTslUa90rBXGvVKw15p1CsbwLdhc2mV5ZPObRiu5duwuXFd2/jukPUF340Ehw37H8H3Qz5v5tZcml5jeMj5rrmcZLPTLs1Q18BgpN7X25ZANB41niCXOhbZ34mVfBYlH5vtiQ902YktGne+cW8u7emAqPRYz7BuYNVGpj7jSKpBg6x0lq1PSpJmIN4fkda5G3JkjV3JbqsuetZU6CHyVY8TLonAYESaKLKbcTI2c+wRQcBDnvMQYj3kFi9hbeyaTSTJKhx7RPrxkMS8RGEPwfHhPOchCnuIWV7y/8PJ3UOg9h6Nxkq17RJRcczLLxJPlZhExDjM1ramm2UPTvj5ys9Xfr7y85Wfr/x85ecrP189e76qP3/q12MoYj3Dk6eHp70nembkIR08PEuz5SV28YVIOPaI1OTlIUw6737aw7HHRKKp60qBTOQC+5+G24cH06cYf3/m6PokI6Q/IPoDoj8gvpYBscmZFqB/rv1z/XrPtR98/ODjB5+WwKgdfEaMZ809VnrPbt7KJkhQANgZdr9VbpdllcAeEW2qk1cseeAq58Awu6Zm2CL9DZ9jaECvHeyRvjn2yt4x+pkxwf8t2kvUqF6eV48TziGBay+HxHbL1jdK9OmxnqCA0CGhCgGhQ0IVAoKj+PWQKgSEtsm1Boea5/A7RbMY7ucufz7151OP8+n9E1mzGQTYMGXDmO2gbAdmQ5QNvfTpJmr08T9mcOabl9fkUP0vM3qi8acawry8dfLwyyDWSbl6PU9GHY5BoZnP1hqZhgQGksR09R/yeovAwKEfm+ykTIYzjsG3zOymldklkxnHoN3MLlukP4NzDCRraXZBXm8RGPSytWyOLZBeOAaSe177ecH/Fz79B3rPoLrgNAAA",
+    filter_url: "{{fl.类型}}{{fl.地区}}{{fl.排序}}{{fl.剧情}}/page/fypage{{fl.年份}}",
+    filter_def: {
+        dianying: {
+            类型: "/id/dianying"
+        },
+        juji: {
+            类型: "/id/juji"
+        },
+        dongman: {
+            类型: "/id/dongman"
+        },
+        zongyi: {
+            类型: "/id/zongyi"
+        },
+        jilupian: {
+            类型: "/id/jilupian"
+        },
+        shuangju: {
+            类型: "/id/shuangju"
+        }
+    },
+    timeout: 5000,
+    class_parse: ".navbar-items li;a&&Text;a&&href;/(\\w+).html",
+    cate_exclude: "更新|热榜",
+    lazy: $js.toString(() => {
+        let js = 'try{function requestApix(callback){$.post(\"api.php\",{vid:getQueryString(\"vid\")},function(result){callback(result.data.url);},\"json\");}requestApix(function(data){location.href=sign(data);})}catch(e){}location.href=document.querySelector(\"#playleft iframe\").src;';
+        input = {
+            parse: 1,
+            url: input,
+            click: js,
+            js: js
+        };
+    }),
+    推荐: "body&&.module:eq(0);*;*;*;*;*",
+    一级: ".module-item;a&&title;img&&data-original;.module-item-note&&Text;a&&href",
+    搜索: "json:list;name;pic;;id"
 }
