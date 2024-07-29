656f58ee81b990d9a4335f36362c9da9d3e167eb
n3rddd <119649988+n3rddd@users.noreply.github.com>
2024-07-29 23:54:40 +0800

    Update 直播.js

--- JN/EXT/JS/直播.js
+++ JN/EXT/JS/直播.js
@@ -1,6 +1,6 @@
 [
 {"name":"雷蒙影视概念宣传片","url":"https://cors.isteed.cc/https://raw.githubusercontent.com/n3rddd/N3RD/master/JN/EXT/LIVE/ctvpv.txt"},
-{"name":"搜集","url":"https://cors.isteed.cc/https://raw.githubusercontent.com/YanG-1989/m3u/main/Gather.m3u","group":1},
+{"name":"电视直播","url":"https://cors.isteed.cc/https://raw.githubusercontent.com/n3rddd/CTVLive/main/live.txt","group":1},
 {"name":"BiliBili直播","url":"https://www.goodiptv.club/bililive.m3u","group":1},
 {"name":"YY轮播","url":"https://www.goodiptv.club/yylunbo.m3u?url=https://lunbo.freetv.top","group":1},
 {"name":"斗鱼一起看","url":"https://live.freetv.top/douyuyqk.m3u","group":1},
