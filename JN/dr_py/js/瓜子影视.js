84f785d50d2e92e1c7e4026e0eae4fcf9d54fccb
n3rddd <119649988+n3rddd@users.noreply.github.com>
2026-06-13 22:56:28 +0800

    Update 瓜子影视.js

--- JN/dr_py/js/瓜子影视.js
+++ JN/dr_py/js/瓜子影视.js
@@ -2,7 +2,8 @@
 
 var rule = {
     title: '瓜子',
-    host: 'https://api.8utdtcq.com',
+    host: 'https://api.w32z7vtd.com',
+   // host: 'https://api.8utdtcq.com',
     url: '/App/IndexList/indexList',
     //   homeUrl: 'http://124.222.116.5/homedata/home.json',
     searchUrl: '/App/Index/findMoreVod#**',
